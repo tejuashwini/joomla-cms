a28ce8f873bc47a2478eca1ba1e28082b27f95ad
Afrooz Rahmati <afr.rahmati@gmail.com>
2021-06-01 16:24:00 -0400

    Update mariadb.php

--- libraries/joomla/database/driver/mariadb.php
+++ libraries/joomla/database/driver/mariadb.php
@@ -1,5 +1,12 @@
-
 <?php
+/**
+ * @package     ${NAMESPACE}
+ * @subpackage
+ *
+ * @copyright   A copyright
+ * @license     A "Slug" license name e.g. GPL2
+ */
+
 /**
  * @package     Joomla.Platform
  * @subpackage  Database
@@ -11,13 +18,12 @@
 defined('JPATH_PLATFORM') or die;
 
 /**
- * MySQL database driver
+ * MySQLi database driver
  *
- * @link        https://dev.mysql.com/doc/
- * @since       3.0.0
- * @deprecated  4.0  Use MySQLi or PDO MySQL instead
+ * @link   https://mariadb.com/
+ * @since  3.0.0
  */
-class JDatabaseDriverMysql extends JDatabaseDriverMysqli
+class JDatabaseDriverMariaDB extends JDatabaseDriver
 {
 	/**
 	 * The name of the database driver.
@@ -25,31 +31,65 @@ class JDatabaseDriverMysql extends JDatabaseDriverMysqli
 	 * @var    string
 	 * @since  3.0.0
 	 */
-	public $name = 'mysql';
+	public $name = 'mariadb';
+
+	/**
+	 * The type of the database server family supported by this driver.
+	 *
+	 * @var    string
+	 * @since  CMS 3.5.0
+	 */
+	public $serverType = 'mariadb';
+
+	/**
+	 * @var    mariadb  The database connection resource.
+	 * @since  1.7.0
+	 */
+	protected $connection;
+
+	/**
+	 * The character(s) used to quote SQL statement names such as table names or field names,
+	 * etc. The child classes should define this as necessary.  If a single character string the
+	 * same character is used for both sides of the quoted name, else the first character will be
+	 * used for the opening quote and the second for the closing quote.
+	 *
+	 * @var    string
+	 * @since  3.0.1
+	 */
+	protected $nameQuote = '`';
+
+	/**
+	 * The null or zero representation of a timestamp for the database driver.  This should be
+	 * defined in child classes to hold the appropriate value for the engine.
+	 *
+	 * @var    string
+	 * @since  3.0.1
+	 */
+	protected $nullDate = '1000-01-01 00:00:00';
+
+	/**
+	 * @var    string  The minimum supported database version.
+	 * @since  3.0.1
+	 */
+	protected static $dbMinimum = '5.0.4';
 
 	/**
 	 * Constructor.
 	 *
-	 * @param   array  $options  Array of database options with keys: host, user, password, database, select.
+	 * @param array $options List of options used to configure the connection
 	 *
 	 * @since   3.0.0
 	 */
 	public function __construct($options)
 	{
-		// PHP's `mysql` extension is not present in PHP 7, block instantiation in this environment
-		if (PHP_MAJOR_VERSION >= 7)
-		{
-			throw new JDatabaseExceptionUnsupported(
-				'This driver is unsupported in PHP 7, please use the MySQLi or PDO MySQL driver instead.'
-			);
-		}
-
 		// Get some basic values from the options.
 		$options['host'] = (isset($options['host'])) ? $options['host'] : 'localhost';
 		$options['user'] = (isset($options['user'])) ? $options['user'] : '';
 		$options['password'] = (isset($options['password'])) ? $options['password'] : '';
 		$options['database'] = (isset($options['database'])) ? $options['database'] : '';
-		$options['select'] = (isset($options['select'])) ? (bool) $options['select'] : true;
+		$options['select'] = (isset($options['select'])) ? (bool)$options['select'] : true;
+		$options['port'] = (isset($options['port'])) ? (int)$options['port'] : null;
+		$options['socket'] = (isset($options['socket'])) ? $options['socket'] : null;
 
 		// Finalize initialisation.
 		parent::__construct($options);
@@ -60,54 +100,94 @@ class JDatabaseDriverMysql extends JDatabaseDriverMysqli
 	 *
 	 * @return  void  Returns void if the database connected successfully.
 	 *
-	 * @since   3.0.0
 	 * @throws  RuntimeException
+	 * @since   3.0.0
 	 */
 	public function connect()
 	{
-		if ($this->connection)
-		{
+		if ($this->connection) {
 			return;
 		}
 
-		// Make sure the MySQL extension for PHP is installed and enabled.
-		if (!self::isSupported())
-		{
-			throw new JDatabaseExceptionUnsupported('Make sure the MySQL extension for PHP is installed and enabled.');
+		/*
+		 * Unlike mariadb_connect() takes the port and socket as separate arguments. Therefore, we
+		 * have to extract them from the host string.
+		 * Port 3308 is defined on my system for MariaDB, you need to change it on your system or provide additional box for port
+		 */
+		$port = isset($this->options['port']) ? $this->options['port'] : 3308;
+		$regex = '/^(?P<host>((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?))(:(?P<port>.+))?$/';
+
+		if (preg_match($regex, $this->options['host'], $matches)) {
+			// It's an IPv4 address with or without port
+			$this->options['host'] = $matches['host'];
+
+			if (!empty($matches['port'])) {
+				$port = $matches['port'];
+			}
+		} elseif (preg_match('/^(?P<host>\[.*\])(:(?P<port>.+))?$/', $this->options['host'], $matches)) {
+			// We assume square-bracketed IPv6 address with or without port, e.g. [fe80:102::2%eth1]:3306
+			$this->options['host'] = $matches['host'];
+
+			if (!empty($matches['port'])) {
+				$port = $matches['port'];
+			}
+		} elseif (preg_match('/^(?P<host>(\w+:\/{2,3})?[a-z0-9\.\-]+)(:(?P<port>[^:]+))?$/i', $this->options['host'], $matches)) {
+			// Named host (e.g example.com or localhost) with or without port
+			$this->options['host'] = $matches['host'];
+
+			if (!empty($matches['port'])) {
+				$port = $matches['port'];
+			}
+		} elseif (preg_match('/^:(?P<port>[^:]+)$/', $this->options['host'], $matches)) {
+			// Empty host, just port, e.g. ':3308'
+			$this->options['host'] = 'localhost';
+			$port = $matches['port'];
+		}
+		// ... else we assume normal (naked) IPv6 address, so host and port stay as they are or default
+
+		// Get the port number or socket name
+		if (is_numeric($port)) {
+			$this->options['port'] = (int)$port;
+		} else {
+			$this->options['socket'] = $port;
 		}
 
+		// Make sure the mariadb extension for PHP is installed and enabled.
+		if (!self::isSupported()) {
+			throw new JDatabaseExceptionUnsupported('The MySQLi extension for PHP is not installed or enabled.');
+		}
+
+		$this->connection = @mysqli_connect(
+			$this->options['host'], $this->options['user'], $this->options['password'], null, $this->options['port'], $this->options['socket']
+		);
+
 		// Attempt to connect to the server.
-		if (!($this->connection = @ mysql_connect($this->options['host'], $this->options['user'], $this->options['password'], true)))
-		{
+		if (!$this->connection) {
 			throw new JDatabaseExceptionConnecting('Could not connect to MySQL server.');
 		}
 
 		// Set sql_mode to non_strict mode
-		mysql_query("SET @@SESSION.sql_mode = '';", $this->connection);
+		mysqli_query($this->connection, "SET @@SESSION.sql_mode = '';");
 
 		// If auto-select is enabled select the given database.
-		if ($this->options['select'] && !empty($this->options['database']))
-		{
+		if ($this->options['select'] && !empty($this->options['database'])) {
 			$this->select($this->options['database']);
 		}
 
 		// Pre-populate the UTF-8 Multibyte compatibility flag based on server version
 		$this->utf8mb4 = $this->serverClaimsUtf8mb4Support();
 
-		// Set the character set (needed for MySQL 4.1.2+).
+		// Set the character set (needed for mariaDB 4.1.2+).
 		$this->utf = $this->setUtf();
 
 		// Disable query cache and turn profiling ON in debug mode.
-		if ($this->debug)
-		{
-			if ($this->hasQueryCacheEnabled())
-			{
-				mysql_query('SET query_cache_type = 0;', $this->connection);
+		if ($this->debug) {
+			if ($this->hasQueryCacheEnabled()) {
+				mysqli_query($this->connection, 'SET query_cache_type = 0;');
 			}
 
-			if ($this->hasProfiling())
-			{
-				mysql_query('SET profiling_history_size = 100, profiling = 1;', $this->connection);
+			if ($this->hasProfiling()) {
+				mysqli_query($this->connection, 'SET profiling_history_size = 100, profiling = 1;');
 			}
 		}
 	}
@@ -122,14 +202,12 @@ class JDatabaseDriverMysql extends JDatabaseDriverMysqli
 	public function disconnect()
 	{
 		// Close the connection.
-		if (is_resource($this->connection))
-		{
-			foreach ($this->disconnectHandlers as $h)
-			{
-				call_user_func_array($h, array( &$this));
+		if ($this->connection instanceof mysqli && $this->connection->stat() !== false) {
+			foreach ($this->disconnectHandlers as $h) {
+				call_user_func_array($h, array(&$this));
 			}
 
-			mysql_close($this->connection);
+			mysqli_close($this->connection);
 		}
 
 		$this->connection = null;
@@ -138,8 +216,8 @@ class JDatabaseDriverMysql extends JDatabaseDriverMysqli
 	/**
 	 * Method to escape a string for usage in an SQL statement.
 	 *
-	 * @param   string   $text   The string to be escaped.
-	 * @param   boolean  $extra  Optional parameter to provide extra escaping.
+	 * @param string $text The string to be escaped.
+	 * @param boolean $extra Optional parameter to provide extra escaping.
 	 *
 	 * @return  string  The escaped string.
 	 *
@@ -147,23 +225,20 @@ class JDatabaseDriverMysql extends JDatabaseDriverMysqli
 	 */
 	public function escape($text, $extra = false)
 	{
-		if (is_int($text))
-		{
+		if (is_int($text)) {
 			return $text;
 		}
 
-		if (is_float($text))
-		{
+		if (is_float($text)) {
 			// Force the dot as a decimal point.
 			return str_replace(',', '.', $text);
 		}
 
 		$this->connect();
 
-		$result = mysql_real_escape_string($text, $this->getConnection());
+		$result = mysqli_real_escape_string($this->getConnection(), $text);
 
-		if ($extra)
-		{
+		if ($extra) {
 			$result = addcslashes($result, '%_');
 		}
 
@@ -179,7 +254,7 @@ class JDatabaseDriverMysql extends JDatabaseDriverMysqli
 	 */
 	public static function isSupported()
 	{
-		return PHP_MAJOR_VERSION < 7 && function_exists('mysql_connect');
+		return function_exists('mysqli_connect');
 	}
 
 	/**
@@ -191,12 +266,35 @@ class JDatabaseDriverMysql extends JDatabaseDriverMysqli
 	 */
 	public function connected()
 	{
-		if (is_resource($this->connection))
-		{
-			return @mysql_ping($this->connection);
+		if (is_object($this->connection)) {
+			return mysqli_ping($this->connection);
 		}
 
 		return false;
+	}
+
+	/**
+	 * Drops a table from the database.
+	 *
+	 * @param string $tableName The name of the database table to drop.
+	 * @param boolean $ifExists Optionally specify that the table must exist before it is dropped.
+	 *
+	 * @return  JDatabaseDriverMysqli  Returns this object to support chaining.
+	 *
+	 * @throws  RuntimeException
+	 * @since   3.0.1
+	 */
+	public function dropTable($tableName, $ifExists = true)
+	{
+		$this->connect();
+
+		$query = $this->getQuery(true);
+
+		$this->setQuery('DROP TABLE ' . ($ifExists ? 'IF EXISTS ' : '') . $query->quoteName($tableName));
+
+		$this->execute();
+
+		return $this;
 	}
 
 	/**
@@ -210,7 +308,51 @@ class JDatabaseDriverMysql extends JDatabaseDriverMysqli
 	{
 		$this->connect();
 
-		return mysql_affected_rows($this->connection);
+		return mysqli_affected_rows($this->connection);
+	}
+
+	/**
+	 * Method to get the database collation.
+	 *
+	 * @return  mixed  The collation in use by the database (string) or boolean false if not supported.
+	 *
+	 * @throws  RuntimeException
+	 * @since   3.0.1
+	 */
+	public function getCollation()
+	{
+		$this->connect();
+
+		// Attempt to get the database collation by accessing the server system variable.
+		$this->setQuery('SHOW VARIABLES LIKE "collation_database"');
+		$result = $this->loadObject();
+
+		if (property_exists($result, 'Value')) {
+			return $result->Value;
+		} else {
+			return false;
+		}
+	}
+
+	/**
+	 * Method to get the database connection collation, as reported by the driver. If the connector doesn't support
+	 * reporting this value please return an empty string.
+	 *
+	 * @return  string
+	 */
+	public function getConnectionCollation()
+	{
+		$this->connect();
+
+		// Attempt to get the database collation by accessing the server system variable.
+		$this->setQuery('SHOW VARIABLES LIKE "collation_connection"');
+		$result = $this->loadObject();
+
+		if (property_exists($result, 'Value')) {
+			return $result->Value;
+		} else {
+			return false;
+		}
 	}
 
 	/**
@@ -218,7 +360,7 @@ class JDatabaseDriverMysql extends JDatabaseDriverMysqli
 	 * This command is only valid for statements like SELECT or SHOW that return an actual result set.
 	 * To retrieve the number of rows affected by an INSERT, UPDATE, REPLACE or DELETE query, use getAffectedRows().
 	 *
-	 * @param   resource  $cursor  An optional database cursor resource to extract the row count from.
+	 * @param resource $cursor An optional database cursor resource to extract the row count from.
 	 *
 	 * @return  integer   The number of returned rows.
 	 *
@@ -226,141 +368,72 @@ class JDatabaseDriverMysql extends JDatabaseDriverMysqli
 	 */
 	public function getNumRows($cursor = null)
 	{
-		$this->connect();
-
-		return mysql_num_rows($cursor ? $cursor : $this->cursor);
+		return mysqli_num_rows($cursor ? $cursor : $this->cursor);
 	}
 
 	/**
-	 * Get the version of the database connector.
+	 * Shows the table CREATE statement that creates the given tables.
 	 *
-	 * @return  string  The database connector version.
+	 * @param mixed $tables A table name or a list of table names.
 	 *
-	 * @since   3.0.0
-	 */
-	public function getVersion()
-	{
-		$this->connect();
-
-		return mysql_get_server_info($this->connection);
-	}
-
-	/**
-	 * Method to get the auto-incremented value from the last INSERT statement.
+	 * @return  array  A list of the create SQL for the tables.
 	 *
-	 * @return  integer  The value of the auto-increment field from the last inserted row.
-	 *
-	 * @since   3.0.0
-	 */
-	public function insertid()
-	{
-		$this->connect();
-
-		return mysql_insert_id($this->connection);
-	}
-
-	/**
-	 * Execute the SQL statement.
-	 *
-	 * @return  mixed  A database cursor resource on success, boolean false on failure.
-	 *
-	 * @since   3.0.0
 	 * @throws  RuntimeException
+	 * @since   3.0.0
 	 */
-	public function execute()
+	public function getTableCreate($tables)
 	{
 		$this->connect();
 
-		// Take a local copy so that we don't modify the original query and cause issues later
-		$query = $this->replacePrefix((string) $this->sql);
+		$result = array();
 
-		if (!($this->sql instanceof JDatabaseQuery) && ($this->limit > 0 || $this->offset > 0))
-		{
-			$query .= ' LIMIT ' . $this->offset . ', ' . $this->limit;
+		// Sanitize input to an array and iterate over the list.
+		settype($tables, 'array');
+
+		foreach ($tables as $table) {
+			// Set the query to get the table CREATE statement.
+			$this->setQuery('SHOW CREATE table ' . $this->quoteName($this->escape($table)));
+			$row = $this->loadRow();
+
+			// Populate the result array based on the create statements.
+			$result[$table] = $row[1];
 		}
 
-		if (!is_resource($this->connection))
-		{
-			JLog::add(JText::sprintf('JLIB_DATABASE_QUERY_FAILED', $this->errorNum, $this->errorMsg), JLog::ERROR, 'database');
-			throw new JDatabaseExceptionExecuting($query, $this->errorMsg, $this->errorNum);
-		}
+		return $result;
+	}
 
-		// Increment the query counter.
-		$this->count++;
+	/**
+	 * Retrieves field information about a given table.
+	 *
+	 * @param string $table The name of the database table.
+	 * @param boolean $typeOnly True to only return field types.
+	 *
+	 * @return  array  An array of fields for the database table.
+	 *
+	 * @throws  RuntimeException
+	 * @since   3.0.1
+	 */
+	public function getTableColumns($table, $typeOnly = true)
+	{
+		$this->connect();
 
-		// Reset the error values.
-		$this->errorNum = 0;
-		$this->errorMsg = '';
+		$result = array();
 
-		// If debugging is enabled then let's log the query.
-		if ($this->debug)
-		{
-			// Add the query to the object queue.
-			$this->log[] = $query;
+		// Set the query to get the table fields statement.
+		$this->setQuery('SHOW FULL COLUMNS FROM ' . $this->quoteName($this->escape($table)));
+		$fields = $this->loadObjectList();
 
-			JLog::add($query, JLog::DEBUG, 'databasequery');
-
-			$this->timings[] = microtime(true);
-		}
-
-		// Execute the query. Error suppression is used here to prevent warnings/notices that the connection has been lost.
-		$this->cursor = @mysql_query($query, $this->connection);
-
-		if ($this->debug)
-		{
-			$this->timings[] = microtime(true);
-
-			if (defined('DEBUG_BACKTRACE_IGNORE_ARGS'))
-			{
-				$this->callStacks[] = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS);
+		// If we only want the type as the value add just that to the list.
+		if ($typeOnly) {
+			foreach ($fields as $field) {
+				$result[$field->Field] = preg_replace('/[(0-9)]/', '', $field->Type);
 			}
-			else
-			{
-				$this->callStacks[] = debug_backtrace();
-			}
-		}
-
-		// If an error occurred handle it.
-		if (!$this->cursor)
-		{
-			// Get the error number and message before we execute any more queries.
-			$this->errorNum = $this->getErrorNumber();
-			$this->errorMsg = $this->getErrorMessage();
-
-			// Check if the server was disconnected.
-			if (!$this->connected())
-			{
-				try
-				{
-					// Attempt to reconnect.
-					$this->connection = null;
-					$this->connect();
-				}
-				// If connect fails, ignore that exception and throw the normal exception.
-				catch (RuntimeException $e)
-				{
-					// Get the error number and message.
-					$this->errorNum = $this->getErrorNumber();
-					$this->errorMsg = $this->getErrorMessage();
-
-					// Throw the normal query exception.
-					JLog::add(JText::sprintf('JLIB_DATABASE_QUERY_FAILED', $this->errorNum, $this->errorMsg), JLog::ERROR, 'database-error');
-
-					throw new JDatabaseExceptionExecuting($query, $this->errorMsg, $this->errorNum, $e);
-				}
-
-				// Since we were able to reconnect, run the query again.
-				return $this->execute();
-			}
-			// The server was not disconnected.
-			else
-			{
-				// Throw the normal query exception.
-				JLog::add(JText::sprintf('JLIB_DATABASE_QUERY_FAILED', $this->errorNum, $this->errorMsg), JLog::ERROR, 'database-error');
-
-				throw new JDatabaseExceptionExecuting($query, $this->errorMsg, $this->errorNum);
+		} // If we want the whole field data object add that to the list.
+		else {
+			foreach ($fields as $field) {
+				$result[$field->Field] = $field;
 			}
 		}
 
-		return $this->cursor;
+		return $result;
 	}
